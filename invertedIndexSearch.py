a801fa21f77929867f8935240831340102fefa83
Harshwardhan Tiwari <tiwariharshwardhhan@gmail.com>
2026-08-05 10:45:04 +0530

    inverted index simple implementation

---
+++ invertedIndexSearch.py
@@ -0,0 +1,41 @@
+documents = {
+    1: "apple banana",
+    2: "banana orange",
+    3: "apple orange apple"
+}
+
+# this is the documents that we have let's say
+# we are implementing a simple inverted index search machine here --> for building the understanding
+# we are going to use a dictionary to store the inverted index
+
+
+inverted_index = {}
+
+for doc_id , content in documents.items() :
+    words = content.lower().split()
+
+    for word in words : 
+        if word in inverted_index : 
+            inverted_index[word].append(doc_id)
+        else : 
+            inverted_index[word] = [doc_id]
+
+
+print(inverted_index)
+
+query = "apple"
+
+results = inverted_index.get(query, [])
+
+print(results)
+
+
+
+
+# this is the output we are searching for 
+
+# {
+#  'apple': [1, 3],
+#  'banana': [1, 2],
+#  'orange': [2, 3]
+# }
